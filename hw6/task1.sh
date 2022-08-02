0b75b22294292103d0ec4a83af6018fa18171311
gtashiro <gtashiro@localhost.localdomain>
2022-08-01 21:53:28 -0700

    task1.sh updated

--- hw6/task1.sh
+++ hw6/task1.sh
@@ -17,6 +17,6 @@ function problem2 {
 }
 
 function problem3 {
-  # Type your answer to problem #3 below this line
-  grep -E '^([A-Z]+[a-z]+\s)([A-Z]+[a-z])' names.txt 
+  # Type your answer to problem #3 below this line 
+  grep -E '^([A-Z][a-z]+\s)([A-Z][a-z]+$)' names.txt
 }
