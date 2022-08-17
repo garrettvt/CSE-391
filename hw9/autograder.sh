8015c96525bdca24ccec3603d9178aaf6cc06b5d
gtashiro <gtashiro@localhost.localdomain>
2022-08-16 22:18:30 -0700

    Updated autograder.sh and output files. Delete on github

--- hw9/autograder.sh
+++ hw9/autograder.sh
@@ -8,21 +8,41 @@
 # Check to see if the number of arguments that is passed
 # is less than 1. 
 if [ "$#" -lt 1 ]; then
-		echo "Usage: ./autograder.sh MAXPOINTS"
+	echo "Usage: ./autograder.sh MAXPOINTS"
 fi
 
+echo "Autograding CSE 391 Homework"
+echo "Grading with a max score of $1"
+echo
+
+expected=$(<expected.txt)
+
 for kid in $(ls students/); do
-		(
-		echo "The $count file is $kid"
-		 
-	        cd ./students/$kid/
+	
+	echo "Processing $kid ..."
 
-		if [ -f "task1.sh" ]; then
-				echo "$kid has done it"
+	(
+       	cd ./students/$kid/
+
+	if [ -f "task1.sh" ]; then
+		./task1.sh > output.txt
+		missed=$(diff -w output.txt <(echo "$expected") | grep "^>" | wc -l)
+		if [ "$missed" -gt 0 ]; then
+			echo "$kid has incorrect output ($missed lines do not match)"
+			let score="$1 - ($missed * 5)"
+			if [ "$score" -le 0 ]; then
+				echo "$kid has earned a score of 0 / $1"
+			else
+				echo "$kid has earned a score of $score / $1"
+			fi
 		else
-				echo "$kid fails"
+			echo "$kid has correct output"
+			echo "$kid has earned a score of $1 / $1"
 		fi
+	else
+		echo "$kid did not turn in the assignment"
+	fi
 
-		echo
-		)
+	echo
+	)
 done
